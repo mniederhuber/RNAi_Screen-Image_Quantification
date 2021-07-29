7b7045c167ae377c2c6b2a8eade0f2d8786cb581
Matthew John Niederhuber <mjn15@longleaf-login2.its.unc.edu>
2021-07-29 15:23:39 -0400

    refining image_parser.py and some initial R analysis

--- code/image_parser.py
+++ code/image_parser.py
@@ -2,7 +2,7 @@ import numpy as np
 import pandas as pd
 from aicsimageio import AICSImage
 
-from skimage import filters, io, morphology, feature
+from skimage import filters, io, morphology, feature, img_as_ubyte
 from skimage.measure import label, regionprops
 from tifffile import imsave, imread
 
@@ -47,13 +47,19 @@ outdata = {
     'date' : [],
     'scene' : [],
     'shape' : [],
+    'size' : [],
     'stack' : [],
+		'gfp':[],
+    'brDisc':[],
     'brDisc_gfp' : [],
     'brDisc_gfpNEG' : [],
     'KDtoWT' : []
 }
-
+n = 0
 for sample_id in sample_sheet_LL['id']:
+    percent_complete = round(100*(n/len(sample_sheet_LL.id)), 2)
+    print(f"{percent_complete}% complete")
+
     line = sample_sheet_LL.loc[sample_sheet_LL['id'] == sample_id, 'line'].iloc[0]
     date = sample_sheet_LL.loc[sample_sheet_LL['id'] == sample_id, 'date'].iloc[0]
     fp = sample_sheet_LL.loc[sample_sheet_LL['id'] == sample_id, 'fp'].iloc[0]
@@ -64,7 +70,7 @@ for sample_id in sample_sheet_LL['id']:
     #print(im)
     #print(im.scenes)
     for scene in im.scenes:
-        print(sample_id, scene)
+      #  print(sample_id, scene)
         im.set_scene(scene)
 
         current_scene = im.current_scene
@@ -72,19 +78,21 @@ for sample_id in sample_sheet_LL['id']:
         array = im.data
         sample = sample_id
         stack = im.shape[2]        
+        size = f"{im.shape[3]}x{im.shape[4]}"
+
         if im.shape[1] == 3:
             dapi = np.amax(im.data[:,0,:,:,:], axis = 1)
             gfp = np.amax(im.data[:,1,:,:,:], axis = 1)
             brDisc = np.amax(im.data[:,2,:,:,:], axis = 1)
             
-            print(dapi.shape)
+           # print(dapi.shape)
             dapi_gauss = filters.gaussian(dapi, sigma = 5)
             dapi_mask = dapi_gauss >= filters.threshold_otsu(dapi_gauss)
 
             gfp_gauss = filters.gaussian(gfp, sigma = 5)
             gfp_mask = gfp_gauss >= filters.threshold_otsu(gfp_gauss)             
 
-            gfpNEG_mask = np.array(np.subtract(dapi_mask, gfp_mask, dtype = np.float), dtype = bool)
+            gfpNEG_mask = np.array(np.subtract(dapi_mask, gfp_mask, dtype = float), dtype = bool)
 
             brDisc_gfp = np.zeros_like(brDisc)
             brDisc_gfp[gfp_mask] = brDisc[gfp_mask]
@@ -95,20 +103,28 @@ for sample_id in sample_sheet_LL['id']:
             brDisc_gfpNEG_signal = np.mean(brDisc_gfpNEG)
 
             KDtoWT = brDisc_gfp_signal / brDisc_gfpNEG_signal
-
-            io.imsave(f"output/{sample}_{scene}_GFP-MASK_MAX.tiff", gfp_mask)
-            io.imsave(f"output/{sample}_{scene}_GFPNEG-MASK_MAX.tiff", gfpNEG_mask)
-            io.imsave(f"output/{sample}_{scene}_DAPI-MASK_MAX.tiff", dapi_mask)
+            
+            io.imsave(f"output/{sample}_{scene}_GFP_MAX.tiff", gfp)
+            io.imsave(f"output/{sample}_{scene}_GFP-MASK_MAX.tiff", img_as_ubyte(gfp_mask))
+            io.imsave(f"output/{sample}_{scene}_GFPNEG-MASK_MAX.tiff", img_as_ubyte(gfpNEG_mask))
+            io.imsave(f"output/{sample}_{scene}_DAPI_MAX.tiff", dapi)
+            io.imsave(f"output/{sample}_{scene}_DAPI-MASK_MAX.tiff", img_as_ubyte(dapi_mask))
+            io.imsave(f"output/{sample}_{scene}_brDisc_MAX.tiff", brDisc)
             
             outdata['symbol'].append(symbol)
             outdata['line'].append(line)
             outdata['date'].append(date)
             outdata['scene'].append(scene)
             outdata['shape'].append(shape)
+            outdata['size'].append(size)
             outdata['stack'].append(stack)
+            outdata['gfp'].append(np.mean(gfp))
+            outdata['brDisc'].append(np.mean(brDisc))
             outdata['brDisc_gfp'].append(brDisc_gfp_signal)
             outdata['brDisc_gfpNEG'].append(brDisc_gfpNEG_signal)
             outdata['KDtoWT'].append(KDtoWT)
+
+    n = n + 1
 
 df = pd.DataFrame(outdata)
 pd.DataFrame.to_csv(df, 'output/outdata.csv')
